1a246bef0c25f31135b871cbb41c5585dde8000d
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-26 20:35:31 -0300

    feat: Repository informations styled

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
-import { Container } from './styles';
 
-export default function Repository({ match }) {
+import Container from '../../components/Container';
+import { Loading, Owner } from './styles';
+
+function Repository({ match }) {
   const [repository, setRepository] = useState({});
   const [issues, setIssues] = useState({});
   const [loading, setLoading] = useState({});
@@ -29,13 +33,32 @@ export default function Repository({ match }) {
     Promise.all([getRepositoryInformation(), getRepositoryIssues()]).then(
       response => {
         const [repository, issues] = response;
-        setRepository(repository);
-        setIssues(issues);
 
+        setRepository(repository.data);
+        setIssues(issues.data);
         setLoading(false);
       }
     );
   }, []);
 
-  return <Container>Repository</Container>;
+  return loading ? (
+    <Loading>Carregando</Loading>
+  ) : (
+    <Container>
+      <Owner>
+        <Link to="/">Voltar aos repositórios</Link>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+    </Container>
+  );
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({ repository: PropTypes.string }),
+  }).isRequired,
+};
+
+export default Repository;
