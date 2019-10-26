d0a4fc3a8f2b02827dde91d368c3d68d563b9619
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-26 20:50:57 -0300

    feat: List issues and labels

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 
 function Repository({ match }) {
   const [repository, setRepository] = useState({});
@@ -32,14 +32,14 @@ function Repository({ match }) {
 
     Promise.all([getRepositoryInformation(), getRepositoryIssues()]).then(
       response => {
-        const [repository, issues] = response;
+        const [repositoryResponse, issuesResponse] = response;
 
-        setRepository(repository.data);
-        setIssues(issues.data);
+        setRepository(repositoryResponse.data);
+        setIssues(issuesResponse.data);
         setLoading(false);
       }
     );
-  }, []);
+  }, [match.params.repository]);
 
   return loading ? (
     <Loading>Carregando</Loading>
@@ -51,6 +51,23 @@ function Repository({ match }) {
         <h1>{repository.name}</h1>
         <p>{repository.description}</p>
       </Owner>
+
+      <IssueList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssueList>
     </Container>
   );
 }
