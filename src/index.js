471281da15529992565fc9de3134f20e029dc64e
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-27 00:48:19 -0300

    feat: Add reactotron to debug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
+import './config/ReactotronConfig';
+
 export default function App() {
+  console.tron.log('Hello console');
+
   return (
     <>
       <SafeAreaView>
