4bf935d642e3de0904152720d069e7b99ecc763f
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-27 00:32:16 -0300

    chore: Starting project with prettier and ESLint

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Hello World</Text>
+      </SafeAreaView>
+    </>
+  );
+}
