d0e630d939a82151a66391f651bca50c128c591e
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-27 23:11:01 -0300

    feat: Get users stars in github api

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
+import PropTypes from 'prop-types';
 
 // import { Container } from './styles';
+import api from '../../services/api';
 
-export default function User({navigation}) {
-  console.tron.log(navigation.getParam('user'));
-  return <View />;
+export default class User extends Component {
+  static navigationOptions = ({navigation}) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  // eslint-disable-next-line react/static-property-placement
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const {navigation} = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({stars: response.data});
+  }
+
+  render() {
+    const {stars} = this.state;
+    return <View />;
+  }
 }
