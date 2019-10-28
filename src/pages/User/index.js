535ed20f13bf46cb05ea2bfc615fe4abed3e2c52
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-27 23:34:37 -0300

    feat: List of favorites user repositories

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
 import PropTypes from 'prop-types';
+import {RefreshControl} from 'react-native';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 import api from '../../services/api';
 
 export default class User extends Component {
@@ -19,19 +31,49 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    loading: false,
   };
 
   async componentDidMount() {
     const {navigation} = this.props;
     const user = navigation.getParam('user');
 
+    this.setState({loading: true});
+
     const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({stars: response.data});
+    this.setState({stars: response.data, loading: false});
   }
 
   render() {
-    const {stars} = this.state;
-    return <View />;
+    const {navigation} = this.props;
+    const {stars, loading} = this.state;
+
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{uri: user.avatar}} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyStractor={star => String(star.id)}
+          renderItem={({item}) => (
+            <Starred>
+              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+          refreshControl={<RefreshControl refreshing={loading} />}
+        />
+      </Container>
+    );
   }
 }
